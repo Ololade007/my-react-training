b656ab88e716e51d938992d126d94217836c38bc
ololade1 <abdulafeezogunbanjo@gmail.com>
2022-11-03 09:04:42 +0100

    first commit

---
+++ src/components/Expenseitem.js
@@ -0,0 +1,13 @@
+function Expenseitem() {
+    return (
+        <div>
+            <div>November 2nd 2022</div>
+            <div>
+                <h2> Car Insurance</h2>
+                <div> $ 249.67 </div>
+            </div>
+        </div>
+    );
+}
+
+export default Expenseitem;
